88ebfbc49a9f11d56d3a10bca116333d5a20f755
Andrew Karell <andrew.karell@pipedrive.com>
2016-12-27 21:58:41 +0200

    Nightly build

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const currentDir = __dirname,
-	config = require(currentDir + '/config/config.json'),
+	config = require(`${currentDir}/config/config.json`),
 	express = require('express'),
 	bodyParser = require('body-parser'),
-	player = require(currentDir + '/lib/player.js'),
-	playlist = require(currentDir + '/lib/playlist.js'),
-	server = require(currentDir + '/lib/server.js'),
+	player = require(`${currentDir}/lib/player.js`),
+	playlist = require(`${currentDir}/lib/playlist.js`),
+	server = require(`${currentDir}/lib/server.js`),
+	validator = require(`${currentDir}/lib/validator.js`),
+	logger = require(`${currentDir}/lib/logger.js`),
+	_ = require('lodash'),
 	app = express();
 
 function errorResponse(response, error, data = null) {
+	if (error instanceof Error) {
+		error = error.message;
+	}
+
 	response.status(400).json({
-		'success': false,
-		'data': data,
-		'error': error,
+		success: false,
+		data: data,
+		error: error,
 	});
 }
 function successResponse(response, data = null) {
 	response.status(200).json({
-		'success': true,
-		'data': data,
+		success: true,
+		data: data,
 	});
 }
 function respond(response, error, data = null) {
@@ -30,123 +37,132 @@ function respond(response, error, data = null) {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	'extended': true,
+	extended: true,
 }));
 
 // Public files
 app.use(express.static(__dirname + '/public'));
 
 // API player routes
-app.get('/close-player', function(request, response) {
-	player.close(function(error) {
+app.get('/close-player', (request, response) => {
+	player.close((error) => {
 		respond(response, error);
 	});
 });
-app.get('/volume-down', function(request, response) {
-	player.volumeDown(function(error) {
+app.get('/volume-down', (request, response) => {
+	player.volumeDown((error) => {
 		respond(response, error);
 	});
 });
-app.get('/volume-up', function(request, response) {
-	player.volumeUp(function(error) {
+app.get('/volume-up', (request, response) => {
+	player.volumeUp((error) => {
 		respond(response, error);
 	});
 });
-app.get('/play-pause', function(request, response) {
-	player.playPause(function(error) {
+app.get('/play-pause', (request, response) => {
+	player.playPause((error) => {
 		respond(response, error);
 	});
 });
-app.get('/forward', function(request, response) {
-	player.forward(function(error) {
+app.get('/forward', (request, response) => {
+	player.forward((error) => {
 		respond(response, error);
 	});
 });
-app.get('/back', function(request, response) {
-	player.back(function(error) {
+app.get('/back', (request, response) => {
+	player.back((error) => {
 		respond(response, error);
 	});
 });
 
 // API playlist routes
-app.get('/current-playlist', function(request, response) {
-	playlist.get()
-		.then(function(data) {
-			successResponse(response, data);
-		})
-		.catch(function(error) {
-			errorResponse(response, data);
-		});
+app.get('/current-playlist', (request, response) => {
+	playlist.get().then((data) => {
+		successResponse(response, data);
+	}).catch((error) => {
+		logger.error(error);
+		errorResponse(response, error);
+	});
 });
-app.get('/previous', function(request, response) {
-	playlist.previous()
-		.then(function(data) {
-			successResponse(response, data);
-		})
-		.catch(function(error) {
-			errorResponse(response, data);
-		});
+app.get('/previous', (request, response) => {
+	playlist.previous().then((data) => {
+		successResponse(response, data);
+	}).catch((error) => {
+		logger.error(error);
+		errorResponse(response, error);
+	});
 });
-app.get('/next', function(request, response) {
-	playlist.next()
-		.then(function(data) {
-			successResponse(response, data);
-		})
-		.catch(function(error) {
-			errorResponse(response, data);
-		});
+app.get('/next', (request, response) => {
+	playlist.next().then((data) => {
+		successResponse(response, data);
+	}).catch((error) => {
+		logger.error(error);
+		errorResponse(response, error);
+	});
 });
-app.post('/play-at', function(request, response) {
-	playlist.playAt(request.body.position)
-		.then(function(data) {
-			successResponse(response, data);
-		})
-		.catch(function(error) {
-			errorResponse(response, error);
-		});
+app.post('/play-at', (request, response) => {
+	playlist.playAt(request.body.position).then((data) => {
+		successResponse(response, data);
+	}).catch((error) => {
+		logger.error(error);
+		errorResponse(response, error);
+	});
 });
 
 // API play routes
-app.post('/random', function(request, response) {
-	server.random(request.body.shows, request.body.playlistSize)
-		.then(function(data) {
-			// respond as soon as possible
-			successResponse(response, data);
+app.post('/random', (request, response) => {
+	let params = {};
 
-			if (request.body.enqueue) {
-				playlist.enqueue(data);
-			} else {
-				playlist.set(data)
-					.then(function() {
-						player.playAt(0);
-					});
-			}
-		})
-		.catch(function(error) {
-			errorResponse(response, error);
+	validator.random(request.body).then((data) => {
+		params = data;
+
+		return server.random(params.showNames, params.amount);
+	}).then((data) => {
+		let responseData = [];
+
+		_.forEach(data, (show) => {
+			responseData.push(`${show.showName} - ${show.episodeName}`);
 		});
+
+		// respond as soon as possible
+		successResponse(response, responseData);
+
+		return data;
+	}).catch((error) => {
+		errorResponse(response, error);
+
+		throw error;
+	}).then((data) => {
+		if ('enqueue' === params.method) {
+			playlist.enqueue(data);
+		} else {
+			playlist.set(data).then(() => {
+				playlist.playAt(0);
+			});
+		}
+	}).catch((error) => {
+		logger.error(error);
+	});
 });
-app.post('/browse', function(request, response) {
-	server.browse(request.body.path)
-		.then(function(data) {
-			successResponse(response, data);
-		})
-		.catch(function(error) {
-			errorResponse(response, error);
-		});
+app.post('/browse', (request, response) => {
+	server.browse(request.body.path).then((data) => {
+		successResponse(response, data);
+	}).catch((error) => {
+		logger.error(error);
+		errorResponse(response, error);
+	});
 });
-app.get('/shows', function(request, response) {
-	server.shows()
-		.then(function(data) {
-			successResponse(response, data);
-		})
-		.catch(function(error) {
-			errorResponse(response, error);
-		});
+app.get('/shows', (request, response) => {
+	server.shows().then((data) => {
+		successResponse(response, data);
+	}).catch((error) => {
+		logger.error(error);
+		errorResponse(response, error);
+	});
 });
 
 // Not found handler
-app.get('*', function(request, response) {
+app.get('*', (request, response) => {
 	response.status(404).json({
 		'success': true,
 		'data': null,
@@ -154,6 +170,6 @@ app.get('*', function(request, response) {
 });
 
 // Start web server
-app.listen(config.port, function() {
+app.listen(config.port, () => {
 	console.log('Started server (plain text) on port ' + config.port);
 });
--- public/bower_components/iron-pages/.github/ISSUE_TEMPLATE.md
+++ public/bower_components/iron-pages/.github/ISSUE_TEMPLATE.md
@@ -1,4 +1,4 @@
-<!-- Instructions: https://github.com/PolymerElements/iron-selector/CONTRIBUTING.md#filing-issues -->
+<!-- Instructions: https://github.com/PolymerElements/iron-pages/CONTRIBUTING.md#filing-issues -->
 ### Description
 <!-- Example: The `paper-foo` element causes the page to turn pink when clicked. -->
 
