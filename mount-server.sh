df12ead534d17f407792fbcc893ad71d463b766a
shiznatix <github@shiznatix.com>
2018-04-15 07:08:43 +0000

    updated mount script

--- mount-server.sh
+++ mount-server.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-sudo sshfs -o allow_other,default_permissions pi@pi-main.apt:/media/500gb /media/500gb
-sudo sshfs -o allow_other,default_permissions pi@pi-main.apt:/media/1000gb /media/1000gb
+sudo sshfs -o allow_other,default_permissions pi@pi-main.apt:/media/3000gb /media/3000gb
